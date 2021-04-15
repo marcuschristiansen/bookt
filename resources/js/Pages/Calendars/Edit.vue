7692e3521875e93b48f414b503b407a6de1a6ae9
Marcus Christiansen <christiansen.marcus@gmail.com>
2021-04-15 16:44:41 +0200

    Styling updates

--- resources/js/Pages/Calendars/Edit.vue
+++ resources/js/Pages/Calendars/Edit.vue
@@ -271,7 +271,9 @@
                                                                 </tbody>
                                                                 <tfoot>
                                                                     <tr>
-                                                                        <td>
+                                                                        <td
+                                                                            class="pt-4"
+                                                                        >
                                                                             <button
                                                                                 class="text-gray-500 bg-transparent border border-solid border-gray-500 hover:bg-gray-500 hover:text-white active:bg-gray-800 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                                                                 type="button"
